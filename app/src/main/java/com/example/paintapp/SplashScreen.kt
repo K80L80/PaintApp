241248c12bddac06d55cd5cf757a126e91c713eb
Katie Stokes <katie.stokes@hci.utah.edu>
2024-09-28 00:36:20 -0600

    cleaned up splash screen imports and comments

--- app/src/main/java/com/example/paintapp/SplashScreen.kt
+++ app/src/main/java/com/example/paintapp/SplashScreen.kt
@@ -4,15 +4,10 @@
  *
  */
 package com.example.paintapp
-
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -22,7 +17,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,11 +30,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.paintapp.databinding.ActivitySplashScreenBinding
 import com.example.paintapp.databinding.FragmentSplashScreenBinding
 import kotlinx.coroutines.delay
 
-//TODO: Spencer –– Splash Screen (Animation) Composable
+//Splash Screen (Animation in Composable)
 class SplashScreenFragment : Fragment() {
 
     private var _binding: FragmentSplashScreenBinding? = null
