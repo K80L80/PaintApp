3f0a02a68a245ba1af2e3307042dfe09b10760c2
Katie Stokes <katie.stokes@hci.utah.edu>
2024-10-04 14:04:44 -0600

    bitmap and @ignore to enity class so that field could be used in memory but ignored by the room database since only the path to the file is going in the database and the bitmap data itself is stored on disk in the 'filesDir' a special folder private to this app to store things locally on disk

--- app/src/main/java/com/example/paintapp/DrawEntity.kt
+++ app/src/main/java/com/example/paintapp/DrawEntity.kt
@@ -1,10 +1,13 @@
 package com.example.paintapp
 
+import android.graphics.Bitmap
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
-
+//The database stores the file path only not the bitmap itself, that is stored on disk (in special folder private to this app called 'filesDir)'
 @Entity(tableName = "drawings")
 data class DrawEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val fileName: String // This stores the path or name of the file
+    val fileName: String, // This stores the path or name of the file
+    @Ignore val bitmap: Bitmap? = null //Bitmap ignored by Room, since that's not stored in database, but we need the field in memory still
 )
