ec9fa8946c391d186af548302635ec46d45f0182
Zach Allred <u0607562@utah.edu>
2024-10-27 12:14:38 -0600

    Redid server with exposed library, and added tables plus post.

--- PaintAppServerSide/PaintAppServer/src/main/kotlin/com/example/Models.kt
+++ PaintAppServerSide/PaintAppServer/src/main/kotlin/com/example/Models.kt
@@ -3,14 +3,15 @@ package com.example
 import org.jetbrains.exposed.sql.Table
 
 object User: Table(){
+    val email = varchar("email", 100)
     val uID = varchar("uID",100)
     override val primaryKey = PrimaryKey(uID)
 }
 
 object SharedImage: Table(){
-    val email = reference("email", User.uID)
+    val email = reference("email", User.email)
     val sharedDate = long("sharedDate")
     val fileName = varchar("fileName", 250)
     val imageTitle = varchar("imageTitle", 250)
-    override val primaryKey = PrimaryKey(arrayOf(User.uID,fileName))
+    override val primaryKey = PrimaryKey(arrayOf(User.email,fileName))
 }
