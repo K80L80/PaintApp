17cdc3aba72850259bc8ca562343f6a6edc9480a
Katie Stokes <katie.stokes@hci.utah.edu>
2024-10-30 08:46:34 -0600

    changed name from mainViewModel to LoginViewModel

--- PaintAppClientSide/app/src/main/java/com/example/paintapp/LoginViewModel.kt
+++ PaintAppClientSide/app/src/main/java/com/example/paintapp/LoginViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class LoginViewModel : ViewModel() {
     private val _isLoggedIn = MutableLiveData(false)
     val isLoggedIn: LiveData<Boolean> get() = _isLoggedIn
 
